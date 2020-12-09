cdc93c940c79eefa5ef3b5888d459d45a24ee2e1
yonggyu94 <dydrb2535@github.com>
2020-12-09 05:21:50 -0800

    implement model

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
 import torch.nn.utils.spectral_norm as spectral_norm
 
 
@@ -21,63 +20,90 @@ class WrongNonLinearException(Exception):
         return 'You should choose \'relu\', \'leaky_relu\' or \'tanh\''
 
 
-class AffineBlock(nn.Module):
-    def __init__(self, in_dim, out_dim, n_slope=0.01, norm="SN", non_linear='relu'):
-        super(AffineBlock, self).__init__()
-        layers = []
+class EqualLR:
+    def __init__(self, name):
+        self.name = name
 
-        if norm == "SN":
-            layers.append(spectral_norm(nn.Linear(in_dim, out_dim)))
-        else:
-            layers.append(nn.Linear(in_dim, out_dim))
-            if norm == 'BN':
-                layers.append(nn.BatchNorm1d(out_dim, affine=True))
-            elif norm == 'IN':
-                layers.append(nn.InstanceNorm1d(out_dim, affine=True))
-            elif norm == None: pass
-            else: raise WrongNormException()
+    @staticmethod
+    def apply(module, name):
+        fn = EqualLR(name)
 
-        if non_linear == 'relu':
-            layers.append(nn.ReLU(inplace=True))
-        elif non_linear == 'leaky_relu':
-            layers.append(nn.LeakyReLU(n_slope, inplace=True))
-        elif non_linear == 'sigmoid':
-            layers.append(nn.Sigmoid(inplace=True))
-        elif non_linear == 'tanh':
-            layers.append(nn.Tanh(inplace=True))
-        elif non_linear == None:
-            pass
-        else:
-            raise WrongNonLinearException()
+        weight = getattr(module, name)
+        del module._parameters[name]
+        module.register_parameter(name + '_orig', nn.Parameter(weight.data))
+        module.register_forward_pre_hook(fn)
 
-        self.affine_block = nn.Sequential(*layers)
+        return fn
+
+    def __call__(self, module, input):
+        weight = self.compute_weight(module)
+        setattr(module, self.name, weight)
+
+
+def equal_lr(module, name='weight'):
+    EqualLR.apply(module, name)
+
+    return module
+
+
+class EqualizedConv2d(nn.Module):
+    def __init__(self, in_ch, out_ch, k_size, stride, padding):
+        super(EqualizedConv2d, self).__init__()
+        conv = nn.Conv2d(in_ch, out_ch, k_size, stride, padding)
+        conv.weight.data.normal_()
+        conv.weight.data.zero_()
+        self.conv = conv
 
     def forward(self, x):
-        out = self.affine_block(x)
+        out = self.conv(x)
+        return out
+
+
+class PixelNorm(nn.Module):
+    def __init__(self):
+        super(PixelNorm, self).__init__()
+
+    def forward(self, x):
+        out = x / torch.sqrt(torch.mean(x**2, dim=1, keepdim=True) + 1e-8)
+        return out
+
+
+class StdConcat(nn.Module):
+    def __init__(self):
+        super(StdConcat, self).__init__()
+
+    def forward(self, x):
+        mean_std = torch.mean(x.std(0))
+        mean_std = mean_std.expand(x.size(0), 1, 4, 4)
+        out = torch.cat([x, mean_std], dim=1)
         return out
 
 
 class ConvBlock(nn.Module):
-    def __init__(self, in_dim, out_dim, ksize=3, stride=1, padding=1, n_slope=0.01, downsample=False,
-                 norm='SN', non_linear='leaky_relu'):
+    def __init__(self, in_dim, out_dim, ksize=3, stride=1, padding=1, n_slope=0.01,
+                 norm='SN', non_linear='leaky_relu', equalized=True):
         super(ConvBlock, self).__init__()
         layers = []
 
+        # Select normalization
         if norm == 'SN':
             layers.append(spectral_norm(nn.Conv2d(in_dim, out_dim, kernel_size=ksize, stride=stride, padding=padding)))
         else:
-            layers.append(nn.Conv2d(in_dim, out_dim, kernel_size=ksize, stride=stride, padding=padding))
+            if equalized:
+                layers.append(EqualizedConv2d(in_dim, out_dim, ksize, stride, padding))
+            else:
+                layers.append(nn.Conv2d(in_dim, out_dim, kernel_size=ksize, stride=stride, padding=padding))
 
             if norm == 'BN':
                 layers.append(nn.BatchNorm2d(out_dim, affine=True))
             elif norm == 'IN':
                 layers.append(nn.InstanceNorm2d(out_dim, affien=True))
+            elif norm == 'PN':
+                layers.append(PixelNorm())
             elif norm == None : pass
             else: raise WrongNormException
 
-        if downsample == True:
-            layers.append(nn.AvgPool2d(2))
-
+        # Select activation function
         if non_linear == 'relu':
             layers.append(nn.ReLU(inplace=True))
         elif non_linear == 'leaky_relu':
@@ -96,138 +122,123 @@ class ConvBlock(nn.Module):
         return out
 
 
-class AdaptiveInstanceNorm(nn.Module):
-    def __init__(self, w_dim, in_dim):
-        super(AdaptiveInstanceNorm, self).__init__()
-        self.in_dim = in_dim
-        self.linear = nn.Linear(w_dim, self.in_dim*2)
-        self.instance_norm = nn.InstanceNorm2d(inplace=True)
-
-    def forward(self, x, w):
-        w_out = self.linear(w)
-        out = self.instance_norm(x)         # [B, in_dim, H, W]
-
-        style_mean, style_std = torch.split(w_out, self.in_dim, dim=1)      # [B, in_dim], [B, in_dim]
-        out = out * style_mean.expand_as(out) + style_std.expand_as(out)    # [B, in_dim, H, W]
-        return out
-
-
-class NoiseInjection(nn.Module):
-    def __init__(self, in_dim):
-        super(NoiseInjection, self).__init__()
-        self.weight = nn.Parameter(torch.zeros(1, in_dim, 1, 1))
-
-    def forward(self, x, noise):
-        out = x + self.weight * noise
-        return out
-
-
-class DownscaleBlock(nn.Module):
-    def __init__(self, in_dim, out_dim, n_slope=0.2):
-        super(DownsampleBlock, self).__init__()
-        self.conv1 = ConvBlock(in_dim, in_dim, ksize=3, stride=1, padding=1, n_slope=n_slope, norm=None,
-                               non_linear='leaky_relu')
-        self.conv2 = ConvBlock(in_dim, out_dim, ksize=3, stride=1, padding=1, n_slope=n_slope, downsample=True,
-                               norm=None, non_linear='leaky_relu')
+class Block(nn.Module):
+    def __init__(self, in_dim, out_dim, ksize_1, pad_1, ksize_2, pad_2, norm, n_slope=0.2, non_linear='leaky_relu',
+                 equlized=True):
+        super(Block, self).__init__()
+        self.convblock_1 = ConvBlock(in_dim, out_dim, ksize_1, padding=pad_1,
+                                     n_slope=n_slope, norm=norm, non_linear=non_linear, equalized=equlized)
+        self.convblock_2 = ConvBlock(out_dim, out_dim, ksize_2, padding=pad_2,
+                                     n_slope=n_slope, norm=norm, non_linear=non_linear, equalized=equlized)
 
     def forward(self, x):
-        out = self.conv1(x)
-        out = self.conv2(out)
+        out = self.convblock_1(x)
+        out = self.convblock_2(out)
         return out
 
-
-class SynthesisConstBlock(nn.Module):
-    def __init__(self, w_dim, in_dim, out_dim):
-        super(SynthesisConstBlock, self).__init__()
-        self.noise_inject1 = NoiseInjection(in_dim=in_dim)
-        self.adain1 = AdaptiveInstanceNorm(w_dim, in_dim)
-        self.conv = ConvBlock(in_dim, out_dim, ksize=3, stride=1, padding=1, n_slope=0.2, norm=None,
-                              non_linear='leaky_relu')
-        self.noise_inject2 = NoiseInjection(in_dim=in_dim)
-        self.adain2 = AdaptiveInstanceNorm(w_dim, in_dim)
-
-    def forward(self, x, w, noise):
-        noise1, noise2 = torch.split(noise, 2, dim=1)   # [B, 1, H, W], [B, 1, H, W]
-        out = self.noise_inject1(x, noise1)     # [B, C, H, W]
-        out = self.adain1(out, w)               # [B, C, H, W]  W : 바꿔야할지..?
-        out = self.conv(out)                    # [B, C, H, W]
-        out = self.noise_inject2(out, noise2)   # [B, C, H, W]
-        out = self.adain2(out, w)               # [B, C, H, W]  W : 바꿔야할지..?
-        return out
-
-
-class SynthesisBlock(nn.Module):
-    def __init__(self, w_dim, in_dim, out_dim):
-        super(SynthesisBlock, self).__init__()
-        self.upsample = nn.Upsample(scale_factor=2)
-        self.conv1 = ConvBlock(in_dim, out_dim, ksize=3, stride=1, padding=1, n_slope=0.01, norm="SN",
-                               non_linear='leaky_relu')
-        self.noise_inject1 = NoiseInjection(in_dim=out_dim)
-        self.adain1 = AdaptiveInstanceNorm(w_dim, out_dim)
-
-        self.conv2 = ConvBlock(in_dim, out_dim, ksize=3, stride=1, padding=1, n_slope=0.01, norm="SN",
-                               non_linear='leaky_relu')
-        self.noise_inject2 = NoiseInjection(in_dim=out_dim)
-        self.adain2 = AdaptiveInstanceNorm(w_dim, out_dim)
-
-    def forward(self, x, w, noise):
-        noise1, noise2 = torch.split(noise, 2, dim=1)   # [B, 1, H, W], [B, 1, H, W]
-        out = self.upsample(x)
-        out = self.conv1(out)
-        out = self.noise_inject1(out, noise1)
-        out = self.adain1(out, w)
-
-        out = self.conv2(out)
-        out = self.noise_inject2(out, noise2)
-        out = self.adain2(out, w)
-        return out
-    
 
 class Generator(nn.Module):
-    def __init__(self, out_dim_list, img_size_list, w_dim):
+    def __init__(self, ch_list=[512, 512, 512, 512, 256, 128, 64, 32], n_label=10, n_slope=0.2):
         super(Generator, self).__init__()
-        self.img_size_list = img_size_list
-        progress_blocks = []
-        for idx, out_dim in enumerate(out_dim_list):
-            if idx == 0:
-                progress_blocks.append(SynthesisConstBlock(w_dim, 512, out_dim_list[0]))
+
+        self.label_embed = nn.Embedding(n_label, n_label)
+        self.code_norm = PixelNorm()
+        self.label_embed.weight.data.normal_()
+
+        # self.progress = nn.ModuleList([Block(ch_list[0], ch_list[0], 4, 3, 3, 1, norm="PN"),  # [B, 512, 4, 4]
+        #                                Block(ch_list[0], ch_list[1], 3, 1, 3, 1, norm="PN"),  # [B, 512, 8, 8]
+        #                                Block(ch_list[1], ch_list[2], 3, 1, 3, 1, norm="PN"),  # [B, 512, 16, 16]
+        #                                Block(ch_list[2], ch_list[3], 3, 1, 3, 1, norm="PN"),  # [B, 512, 32, 32]
+        #                                Block(ch_list[3], ch_list[4], 3, 1, 3, 1, norm="PN"),  # [B, 256, 64, 64]
+        #                                Block(ch_list[4], ch_list[5], 3, 1, 3, 1, norm="PN"),  # [B, 128, 128, 128]
+        #                                Block(ch_list[5], ch_list[6], 3, 1, 3, 1, norm="PN"),  # [B, 64, 256, 256]
+        #                                Block(ch_list[6], ch_list[7], 3, 1, 3, 1, norm="PN")   # [B, 32, 512, 512]
+        #                                ])
+
+        # self.to_rgb = nn.ModuleList([nn.Conv2d(512, 3, 1),
+        #                              nn.Conv2d(512, 3, 1),
+        #                              nn.Conv2d(512, 3, 1),
+        #                              nn.Conv2d(512, 3, 1),
+        #                              nn.Conv2d(256, 3, 1),
+        #                              nn.Conv2d(128, 3, 1),
+        #                              nn.Conv2d(64, 3, 1),
+        #                              nn.Conv2d(32, 3, 1)
+        #                              ])
+
+        progress_layers = []
+        to_rgb_layers = []
+        for i in range(len(ch_list)):
+            if i == 0:
+                progress_layers.append(Block(ch_list[i], ch_list[i], 4, 3, 3, 1, norm="PN"))
             else:
-                progress_blocks.append(SynthesisBlock(w_dim, out_dim_list[idx-1], out_dim_list[idx]))
-        to_rgb_blocks = [ConvBlock(out_dim, 3, ksize=1, stride=1, padding=0) for out_dim in out_dim_list]
+                progress_layers.append(Block(ch_list[i-1], ch_list[i], 3, 1, 3, 1, norm="PN"))
+            to_rgb_layers.append(nn.Conv2d(ch_list[i], 3, 1))
 
-        self.progress = nn.ModuleList(*progress_blocks)
-        self.to_rgb = nn.ModuleList(*to_rgb_blocks)
+        self.progress = nn.ModuleList(*progress_layers)
+        self.to_rgb = nn.ModuleList(*to_rgb_layers)
 
-    def forward(self, x, w, step):
-        for idx, progress_block, to_rgb in enumerate(zip(self.progress, self.to_rgb)):
-            noise = torch.randn(x.size(0), 2, img_size_list[idx], img_size_list[idx])
-            x = progress_block(x, w, noise)
-            if idx == step:
-                out = to_rgb(x)
-                return out
+    def forward(self, x, label, step=0, alpha=-1):
+        input = self.code_norm(x)
+        label = self.label_embed(label)
+        out = torch.cat([input, label], 1).unsqueeze(2).unsqueeze(3)
+
+        for i, (block, to_rgb) in enumerate(zip(self.progress, self.to_rgb)):
+            if i > 0 and step > 0:
+                upsample = F.upsample(out, scale_factor=2)
+                out = block(upsample)
+            else:
+                out = block(out)
+
+            if i == step:
+                out = to_rgb(out)
+
+                if i < 0 and 0 <= alpha < 1:
+                    skip_rgb = self.to_rgb[i-1](upsample)
+                    out = (1-alpha)*skip_rgb + alpha*out
+                break
+        return out
 
 
 class Discriminator(nn.Module):
-    def __init__(self, out_dim_list):
+    def __init__(self, ch_list=[512, 512, 512, 512, 256, 128, 64, 32], n_slope=0.2, n_label=10):
         super(Discriminator, self).__init__()
-        progress_blocks = []
-        for idx, out_dim in enumerate(out_dim_list):
-            if idx == 0:
-                progress_blocks.append(DownscaleBlock(512, out_dim_list[idx]))
+        reversed(ch_list)
+
+        self.std_concat = StdConcat()
+
+        progress_layers = []
+        from_rgb_layers = []
+        for i in range(len(ch_list)):
+            if i == (len(ch_list)-1):
+                progress_layers.append(Block(ch_list[i]+1, ch_list[i], 3, 1, 3, 1, norm="SN", equlized=False))
             else:
-                progress_blocks.append(DownscaleBlock(out_dim_list[idx-1], out_dim_list[idx]))
-        from_rgb_blocks = [ConvBlock(3, out_dim, ksize=1, stride=1, padding=0) for out_dim in out_dim_list]
+                progress_layers.append(Block(ch_list[i], ch_list[i+1], 3, 1, 3, 1, norm="SN", equlized=False))
+            from_rgb_layers.append(nn.Conv2d(3, ch_list[i], 1))
 
-        self.progress = nn.ModuleList(*progress_blocks)
-        self.from_rgb = nn.ModuleList(*from_rgb_blocks)
-
-        self.linear = nn.Linear(512, 1)
+        self.progress = nn.ModuleList(*progress_layers)
+        self.from_rgb = nn.ModuleList(*from_rgb_layers)
 
         self.n_layer = len(self.progress)
+        self.linear = nn.Linear(512, 1 + n_label)
 
-    def forward(self, x, step):
-        for idx, progress_block, from_rgb in enumerate(zip(self.progress, self.from_rgb)):
-            if idx == step:
-                out = self.from_rgb(x)
-        return out
+    def forward(self, x, step=0, alpha=-1):
+        step = self.n_layer - 1 - step
+
+        for i in range(step, self.n_layer):
+            out = self.from_rgb[i](x)
+            if i == (self.n_layer-1):
+                out = self.std_concat(out)
+
+            out = self.progress[i](out)
+
+            out = F.avg_pool2d(out, 2)
+            if i == step and 0 <= alpha < 1:
+                skip_rgb = F.avg_pool2d(x, 2)
+                skip_rgb = self.from_rgb[i+1](skip_rgb)
+                out = (1-alpha)*skip_rgb + alpha*out
+
+        out = out.squeeze(3).squeeze(2)
+        out = self.linear(out)
+
+        return out[:, 0], out[:, 1:]
 
